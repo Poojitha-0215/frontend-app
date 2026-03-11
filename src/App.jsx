0c2e74fa6f4b792b16a8a5bd795a1c864e7bebd6
Poojitha0215 <poojitha0215@gmail.com>
2026-03-11 14:23:53 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,27 @@ import Header from "./components/Header";
 import Content from "./components/Content";
 import Footer from "./components/Footer";
 import Login from "./components/Login";
+import Logout from "./components/Logout";
 import Register from "./components/Register";
 import Cart from "./components/Cart";
 import Orders from "./components/Orders";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
 
   return ( 
   <div>
+    <BrowserRouter>
     <Header />
-    <Content />
-    <Login />
-    <Register />
-    <Cart />
-    <Orders />
+    <Routes>
+    <Route index element={<Content />} />
+    <Route path="cart" element={<Cart />} />
+    <Route path="Login" element={<Login />} />
+    <Route path="Logout" element={<Logout />} />
+    <Route path="register" element={<Register />} />
+    <Route path="orders" element={<Orders />} />
+    </Routes>
     <Footer />
-    
+    </BrowserRouter>
     </div>
   );
 }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,15 @@
+import { Link } from "react-router-dom";
 function Login() {
-    return <h1>Login Page</h1>
+    return <div>
+        <h2>Login Page</h2>
+        <p><input type="text" placeholder="Email" /></p>
+        <p><input type="password" placeholder="Password" /></p>
+        <p>
+            <button>Login</button>
+        </p>
+        <p>
+            <Link to="/register">New user register here</Link>
+        </p>
+    </div>
 }
 export default Login;
